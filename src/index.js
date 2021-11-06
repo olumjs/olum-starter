82af91cac65497a4027fad97bef26c811213477a
Eissa Saber <eissapk44@gmail.com>
2021-11-06 03:50:12 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Olum from "olum";
+import App from "./App.js";
+
+new Olum().$("#root").use(App); 
+
+// if ("serviceWorker" in navigator) { // uncomment to enable service worker when deploying
+//   window.on("load", () => navigator.serviceWorker.register("/service-worker.js").catch(console.error));
+// }
