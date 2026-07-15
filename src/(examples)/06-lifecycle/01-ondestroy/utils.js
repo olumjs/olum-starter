cdd5e43e8e2133f7fe98489a3e3a16cae8f022bb
Eissa Saber <eissapk@zohomail.com>
2026-07-15 15:14:13 +0300

    update examples

--- src/(examples)/06-lifecycle/01-ondestroy/utils.js
+++
@@ -1,9 +0,0 @@
-import { onDestroy } from 'olum';
-
-export function onInterval(callback, milliseconds) {
-	const interval = setInterval(callback, milliseconds);
-
-	onDestroy(() => {
-		clearInterval(interval);
-	});
-}
